181a804f31b23bb5868cbf9dd22bf52a64ad2532
Maisa Maximo Ferreira <b.outmaself@gmail.com>
2022-05-07 20:26:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 600) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services .section-title,
+  #services .section-subtitle,
+  #services .card,
+  #about,
+  #about header,
+  #about .content, 
+  #contact, 
+  #contact header, 
+  #contact .content, 
+  footer`)
